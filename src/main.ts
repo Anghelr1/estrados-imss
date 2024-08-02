f436c9232d2bc6f62da8c3fb4ae09044cb813d4b
Anghel <anghelgr14@gmail.com>
2024-08-02 13:57:02 -0600

    feat: Add global exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { AllExceptionsFilter } from './filters/Exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //  Set the global prefix to 'api'
   app.setGlobalPrefix('api');
 
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
+  // Use the AllExceptionsFilter class as the global filter
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
 
   await app.listen(3000);
 }
